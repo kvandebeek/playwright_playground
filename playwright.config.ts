2c2f5d12805c629871a30a0394281a2fed2da419
Kristof Vandebeek <kristof.vandebeek@gmail.com>
2026-02-11 22:33:01 +0100

    Minor changes to URL wrt IPv6 / webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   ],
 
   use: {
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://127.0.0.1:8080',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
@@ -31,7 +31,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npx --yes http-server ./playground -p 8080 -c-1',
-    url: 'http://localhost:8080',
+    url: 'http://127.0.0.1:8080',
     reuseExistingServer: true,
     timeout: 30_000
   }
